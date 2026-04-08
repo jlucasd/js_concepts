2d52925e5bcb33eae806a7ac649ef6f40d87a22b
João Luccas Damiani <jlucasd01@gmail.com>
2026-04-07 23:34:17 -0300

    feat: initialize project with basic HTML structure and JavaScript demonstration scripts

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.getElementById("texto").innerHTML = "Meu primeiro texto <b>JS</b>!";
+alert(10 + 5);
+console.log("Oi isso é um console.log!");
+let a,b,c; a=5;  b=6; c=7;
+let pessoa = "João";
+function minhafuncao(){
+    document.writeln(5 * 5);
+}
