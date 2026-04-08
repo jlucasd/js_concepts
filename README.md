d3bc87b622264f0c00f1ea72c6dd4a04c2d758c3
João Luccas Damiani <jlucasd01@gmail.com>
2026-04-07 23:47:21 -0300

    feat: expand JavaScript examples and add project documentation README

---
+++ README.md
@@ -0,0 +1,7 @@
+Este projeto foi criado para praticar os conceitos fundamentais de JavaScript, incluindo manipulação do DOM, funções, variáveis e muito mais.
+
+## Referências
+- [Programação Web](https://programacaoweb.com.br/)
+- [Curso JS no YouTube](https://www.youtube.com/playlist?list=PL2Fdisxwzt_d590u3uad46W-kHA0PTjjw)
+
+## Desenvolvido por João Luccas Damiani.
--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
 document.getElementById("texto").innerHTML = "Meu primeiro texto <b>JS</b>!";
 alert(10 + 5);
 console.log("Oi isso é um console.log!");
-let a,b,c; a=5;  b=6; c=7;
+let a, b, c; a = 5; b = 6; c = 7;
 let pessoa = "João";
-function minhafuncao(){
+function minhafuncao() {
     document.writeln(5 * 5);
 }
+console.log(5 + 5); // Números
+console.log("5" + "5"); // Strings
+
+// Variáveis
+var d;
+let e;
+const f = 0;
+
+var x = a * b;
+console.log(x);
+
+// Variáveis Case Sensitive
+
+var nome = "João";;
+var NOME = "Luccas";
+
+console.log(nome);
+console.log(NOME);
+
+var primeiroNome = "João";
+var ultimo_nome = "Damiani";
+
